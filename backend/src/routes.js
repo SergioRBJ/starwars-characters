3843c7be7fd0454b4d9f58b5cefef6a3b0be40d0
SergioRBJ <sergiorbj93@gmail.com>
2019-12-18 13:06:52 -0300

    fix: remove actuations from routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,6 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import AvatarController from './app/controllers/AvatarController';
-import ActuationController from './app/controllers/ActuationController';
 import FilmController from './app/controllers/FilmController';
 
 const routes = new Router();
@@ -16,9 +15,6 @@ routes.get('/', (req, res) =>
 
 // Add avatar image to the character
 routes.post('/avatar/:id/:ent/', upload.single('file'), AvatarController.store);
-
-// Get characters who acted in the movie
-routes.get('/actuations/:film/', ActuationController.index);
 
 // Get all films
 routes.get('/films/', FilmController.index);
